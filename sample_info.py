14f7126dd4d2ed62594ed01f552110c499c486d5
ashwinprabhu369 <117912989+ashwinprabhu369@users.noreply.github.com>
2022-12-02 14:08:32 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 9778695
+API_HASH = '1e7c3fab8b4c0076419e9429915f8f7e'
+BOT_TOKEN = '5674869054:AAH9kidFrXkdBlLFG2P2Vs0kzW9tsh-o6hk'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
 CACHE_TIME = 300
-USE_CAPTION_FILTER = False
+USE_CAPTION_FILTER = True
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
-AUTH_USERS = []
-AUTH_CHANNEL = None
+ADMINS = [1931600275]
+CHANNELS = [-1001499759653]
+AUTH_USERS = [1931600275]
+
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'GreyMatter'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb+srv://filtermov:filtermov@cluster0.b8xci.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'Cluster0'
+COLLECTION_NAME = 'Telegram_files'  # If you are using the same database, then use different collection name for each bot
 
 
